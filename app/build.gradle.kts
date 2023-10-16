f68f3e2540f3ec508cdb1902db1894d6d5106ef4
123Duo3 <123duo3@gmail.com>
2023-10-17 00:16:07 +0800

    Add dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
     implementation(libs.navigation)
+    implementation(libs.stalla)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
