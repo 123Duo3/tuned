05d9d25b9cb79f024d626bb5243e8c4e43c01a19
123Duo3 <123duo3@gmail.com>
2026-06-02 15:38:46 +0800

    fix(ui): delegate system bar insets to screens

--- app/src/main/kotlin/ink/duo3/tuned/MainActivity.kt
+++ app/src/main/kotlin/ink/duo3/tuned/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import ink.duo3.tuned.navigation.TunedNavGraph
 import ink.duo3.tuned.ui.theme.TunedTheme
@@ -17,9 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TunedTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TunedNavGraph(modifier = Modifier.padding(innerPadding))
-                }
+                TunedNavGraph(modifier = Modifier.fillMaxSize())
             }
         }
     }
