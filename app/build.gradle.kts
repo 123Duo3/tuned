1aa3e2c303bdd7ffe33ffc3e60dd7b383e18910f
123Duo3 <123duo3@gmail.com>
2026-06-08 16:48:31 +0800

    build(deps): upgrade target to sdk37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,17 +10,12 @@ plugins {
 
 android {
     namespace = "ink.duo3.tuned"
-    compileSdk {
-        version =
-            release(36) {
-                minorApiLevel = 1
-            }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "ink.duo3.tuned"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
