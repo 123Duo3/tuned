e0550cfe64e6769de558cbb8162c6f40240facbf
123Duo3 <123duo3@gmail.com>
2026-06-15 18:14:24 +0800

    style(home): use app download icon

--- app/src/main/kotlin/ink/duo3/tuned/ui/home/RecentlyUpdatedCard.kt
+++ app/src/main/kotlin/ink/duo3/tuned/ui/home/RecentlyUpdatedCard.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.PlaylistAdd
-import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.buildAnnotatedString
@@ -340,7 +340,7 @@ private fun EpisodeActions(
         }
         IconButton(onClick = {}) {
             Icon(
-                imageVector = Icons.Filled.Download,
+                painter = painterResource(R.drawable.ic_download_24dp),
                 contentDescription = stringResource(R.string.home_subscription_download),
             )
         }
